b1d6c4e9a2df954ba22d4b9d200b5d89a5292897
LakshayJakhar <lakshayjakhar880@gmail.com>
2025-05-28 02:05:55 -0400

    stuff

--- src/main.py
+++ src/main.py
@@ -1,2 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World from Railway + CI/CD!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
+
+
+
+
 def some_function():
     return 42
