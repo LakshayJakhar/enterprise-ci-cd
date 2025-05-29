276476a0ca78d188092d5f012923f6b9171a51c4
LakshayJakhar <lakshayjakhar880@gmail.com>
2025-05-29 19:08:45 -0400

    Docker deployment attempt5

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Railway! are you working"
+    return "Hello from Railway! are you working  hello"
 
 
 if __name__ == "__main__":
